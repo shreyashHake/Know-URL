fe5365149c0aae5eb9a5a191570e531f8ae8426a
91808 <shreyashhake4@gmail.com>
2022-01-11 13:08:58 +0530

    UI and working improved

--- app/src/main/java/com/example/javaurlpackage/MainActivity.kt
+++ app/src/main/java/com/example/javaurlpackage/MainActivity.kt
@@ -12,80 +12,84 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val input = findViewById<EditText>(R.id.editText)
+        val ip = input.text.toString()
+
         val host = findViewById<Button>(R.id.button1)
-        host.setOnClickListener { toFindHost() }
+        host.setOnClickListener { toFindHost(ip) }
 
         val port = findViewById<Button>(R.id.button2)
-        port.setOnClickListener { toFindPort() }
+        port.setOnClickListener { toFindPort(ip) }
 
         val path = findViewById<Button>(R.id.button3)
-        path.setOnClickListener { toFindPath() }
+        path.setOnClickListener { toFindPath(ip) }
 
         val protocol = findViewById<Button>(R.id.button4)
-        protocol.setOnClickListener { toFindProtocol() }
+        protocol.setOnClickListener { toFindProtocol(ip) }
+
+        val query = findViewById<Button>(R.id.button5)
+        query.setOnClickListener { toFindQuery(ip) }
+
+        val clear = findViewById<Button>(R.id.clear_text)
+        clear.setOnClickListener { clearURL() }
     }
 
-    private fun toFindHost() {
-        val input = findViewById<EditText>(R.id.url)
-        val result = findViewById<EditText>(R.id.result)
-        val ip = input.text.toString()
-        if (ip == null) {
-            result.setText("")
-            return
-        }
+    private fun clearURL() {
+        val input = findViewById<EditText>(R.id.editText)
+        input.setText("")
+
+        val result = findViewById<EditText>(R.id.resultText)
+        result.setText("")
+    }
+
+    private fun toFindHost(ip : String) {
+        val result = findViewById<EditText>(R.id.resultText)
         try {
             val url = URL(ip)
             result.setText(url.host.toString())
         } catch (e: Exception) {
-            result.setText(e.toString())
+            result.setText(getString(R.string.error))
         }
     }
 
-    private fun toFindPort() {
-        val input = findViewById<EditText>(R.id.url)
-        val result = findViewById<EditText>(R.id.result)
-        val ip = input.text.toString()
-        if (ip == null) {
-            result.setText("")
-            return
-        }
+    private fun toFindPort(ip : String) {
+        val result = findViewById<EditText>(R.id.resultText)
         try {
             val url = URL(ip)
             result.setText(url.port.toString())
         } catch (e: Exception) {
-            result.setText(e.toString())
+            result.setText(getString(R.string.error))
         }
     }
 
-    private fun toFindPath() {
-        val input = findViewById<EditText>(R.id.url)
-        val result = findViewById<EditText>(R.id.result)
-        val ip = input.text.toString()
-        if (ip == null) {
-            result.setText("")
-            return
-        }
+    private fun toFindPath(ip : String) {
+        val result = findViewById<EditText>(R.id.resultText)
         try {
             val url = URL(ip)
             result.setText(url.path.toString())
         } catch (e: Exception) {
-            result.setText(e.toString())
+            result.setText(getString(R.string.error))
         }
     }
 
-    private fun toFindProtocol() {
-        val input = findViewById<EditText>(R.id.url)
-        val result = findViewById<EditText>(R.id.result)
-        val ip = input.text.toString()
-        if (ip == null) {
-            result.setText("")
-            return
-        }
+    private fun toFindProtocol(ip : String) {
+        val result = findViewById<EditText>(R.id.resultText)
         try {
             val url = URL(ip)
             result.setText(url.protocol.toString())
         } catch (e: Exception) {
+            result.setText(getString(R.string.error))
+        }
+    }
+
+    private fun toFindQuery(ip : String) {
+        val result = findViewById<EditText>(R.id.resultText)
+        try {
+            val url = URL(ip)
+            result.setText(url.query.toString())
+        } catch (e: Exception) {
             result.setText(e.toString())
         }
     }
+
 }
